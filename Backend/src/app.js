328f56a3763a91540650c073070dfabcd24390fc
Anurag Ram <ak6367732@gmail.com>
2025-10-07 16:46:58 +0530

    added signup,login,get api

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,21 +1,20 @@
 const express = require('express')
 const connectDB = require('../database/database')
 const app = express()
+const AuthRouter= require('../Routes/AuthRouter')
+const cookieparser=require('cookie-parser')
+
+app.use(express.json())
+app.use(cookieparser())
+
+app.use("/user",AuthRouter)
 
 
 
 
-
-
-
-connectDB().then(()=>{
-    try{
-     console.log("DB connected succesfully")
-     app.listen(3000,()=>{
-     console.log("server is running on port number 3000")
-})
-    }
-    catch(err){
-        console.log(err.message);
-    }
-})
+connectDB()
+    .then(() => {
+        console.log("DB connected successfully");
+        app.listen(3000, () => console.log("Server running on port 3000"));
+    })
+    .catch(err => console.log("DB connection failed:", err.message));
