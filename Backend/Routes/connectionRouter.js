a3329e3174854bf15202ea36321d4e8afb1c096e
Anurag Ram <ak6367732@gmail.com>
2025-10-08 16:17:32 +0530

    added send connection request and accept or reject connection

--- Backend/Routes/connectionRouter.js
+++ Backend/Routes/connectionRouter.js
@@ -4,6 +4,7 @@ const connectionRouter=express.Router();
 const UserAuth=require('../Authentication/userAuth')
 const connectionRequest=require('../models/connectionRequest')
 const User = require('../models/UserSchema');
+const { createHashRouter } = require('react-router');
 
 connectionRouter.post("/request/send/:status/:userId",UserAuth,async(req,res)=>{
 try {
@@ -47,7 +48,46 @@ res.json({
 }
 })
 
+connectionRouter.post("/request/review/:status/:requestId",UserAuth,async(req,res)=>{
+try{
+ 
+    const LoggedInUser=req.user
+    const status=req.params?.status
+    const requestId=req.params?.requestId
 
+
+    const ALLOWED_STATUS=["accepted","rejected"];
+    if(!ALLOWED_STATUS.includes(status)){
+        throw new Error('Invalid Entry')
+    }
+
+
+    const check_connectionRequests = await connectionRequest.findOne({
+        _id:requestId,
+        status:"interested",
+        toUserId:LoggedInUser._id
+        
+    })
+     if(!check_connectionRequests){
+        throw new Error("user is not present")
+    }
+    check_connectionRequests.status=status;
+    const data  = await check_connectionRequests.save();
+    res.json({
+        message:"your connection request list",
+        data:data
+    })
+
+
+}
+catch(err){
+   res.status(400).send(err.message)
+}
+
+
+
+
+})
 
 
 
